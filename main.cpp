101f41d69e2515f30b0372715de4a33bc833d809
Andy <mightydodongo@gmail.com>
2017-11-07 19:50:44 -0600

    Updated refactoring of main.cpp.

--- main.cpp
+++ main.cpp
@@ -7,23 +7,23 @@ int main(int argc, char **argv) {
 
 	//Options//
 	int FPS = 60;
-	bool fullScreen = false; //true if game will run in fullscreen
-	bool debugMode = false; //true if debugMode
-	bool keylogger = false; //true if the keylogger will be run
-	bool debugSingleStep = false; //true if debug mode will happen manually, one frame at a time
-	bool debugDecouple = false; //debug mode with no SDL interaction
-	bool singleThread = false; //game runs with a single thread. No thread pool
+	bool fullScreen = false; //true if game will run in fullscreen//
+	bool debugMode = false; //true if debugMode//
+	bool keylogger = false; //true if the keylogger will be run//
+	bool debugSingleStep = false; //true if debug mode will happen manually, one frame at a time//
+	bool debugDecouple = false; //debug mode with no SDL interaction//
+	bool singleThread = false; //game runs with a single thread. No thread pool//
 
 	//screen dimensions if not fullscreen//
 	int screenWidth = 0;
 	int screenHeight = 0;
 
 	//Read Options from Options.txt. Returns true if Options.txt exists//
-	if (checkOptions(&FPS, &fullScreen, &debugMode, &keylogger, &debugSingleStep, &debugDecouple, &singleThread)) {
-		//set screen resolution for fullscreen
+  if (checkOptions(&FPS, &fullScreen, &debugMode, &keylogger, &debugSingleStep, &debugDecouple, &singleThread)) {
+		//set screen resolution for fullscreen//
 		if (fullScreen) {
 
-			//TODO. ADD This Into "LinuxFunctions.h"
+			//TODO. ADD This Into "LinuxFunctions.h"//
 			Display* disp = XOpenDisplay(NULL);
 			Screen*  scrn = DefaultScreenOfDisplay(disp);
 			screenHeight = scrn->height;
@@ -53,10 +53,13 @@ int main(int argc, char **argv) {
 		screenHeight = 768;
 	}
 
-	//The Orion Engine uses different modes for loops based on initial settings (debug, keylog, etc).
+	//The Orion Engine uses different modes for loops based on initial settings (debug, keylog, etc).//
 	//Intended loop is entered here//
 
-	if (debugMode == false && keylogger == false && debugSingleStep == false && debugDecouple == false) {
+	if (debugMode == false &&
+	keylogger == false &&
+	debugSingleStep == false &&
+	debugDecouple == false) {
 
 		loop(FPS, screenWidth, screenHeight, fullScreen);
 
@@ -66,7 +69,9 @@ int main(int argc, char **argv) {
 		loopWrite(FPS, screenWidth, screenHeight, fullScreen);
 
 	}
-	else if (debugMode == true && keylogger == false && singleThread == false) {
+	else if (debugMode == true &&
+	keylogger == false &&
+	singleThread == false) {
 
 
 		loopDebug(FPS, screenWidth, screenHeight, fullScreen);
@@ -82,7 +87,8 @@ int main(int argc, char **argv) {
 		loopDebug_SDLDecoupled(FPS, screenWidth, screenHeight, fullScreen);
 
 	}
-	else if (debugMode == true && keylogger == false && singleThread == true) {
+	else if (debugMode == true && keylogger == false &&
+	singleThread == true) {
 
 		loopDebugSingleThreaded(FPS, screenWidth, screenHeight, fullScreen);
 
